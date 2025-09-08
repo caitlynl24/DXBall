58f0016ab2c0f2e004ebff31f7d6b008588722bb
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-09-08 11:19:01 -0400

    Added ball
    Ball moves but goes through paddle

---
+++ DXBall/Assets/Scripts/ball.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ball_cs : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb;
+    public float speed;
+    public Vector2 direction;
+    void Start()
+    {
+       rb=GetComponent<Rigidbody2D>();
+       //(1,1)
+       direction=Vector2.one.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity=direction*speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("paddle"))
+        {
+            direction.y = -direction.y;
+        }
+    }
+}
