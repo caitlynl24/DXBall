dea7ba4fec224784466630c64b2b363321ec7005
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-09-26 19:20:39 -0400

    Added Main Menu
    Added a main menu with buttons to take you to level1 or level2

---
+++ DXBall/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    } 
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
